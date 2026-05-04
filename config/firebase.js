2f6a5ace25aff98b17a7486b641b12475cd5a1d1
hanshyam <ghanshyampatidar3011@gmail.com>
2026-05-04 14:52:26 +0530

    adding to the firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,23 @@
 import admin from "firebase-admin";
-import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("../config/serviceAccountKey.json", import.meta.url))
-);
+let serviceAccount;
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing in env");
+}
+
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+
+
+  serviceAccount.private_key =
+    serviceAccount.private_key.replace(/\\n/g, "\n");
+} catch (err) {
+  throw new Error("Invalid FIREBASE_SERVICE_ACCOUNT JSON");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
