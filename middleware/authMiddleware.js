040e2ac0879f6779068f9f1d9d7162069936e288
hanshyam <ghanshyampatidar3011@gmail.com>
2026-05-09 15:11:27 +0530

    updating the import

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import admin from "../config/firebaseAdmin.js";
+import admin from '../config/firebase.js';
 import asyncHandler from "express-async-handler";
 import userModel from "../models/userModal.js";
 
